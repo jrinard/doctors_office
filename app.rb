1b82e40df12cbf05db2d578660eb363aa86451a4
Cody Brubaker and Joshua Rinard <cody@>
2016-12-06 14:27:24 -0800

    successfully add patients to doctors

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require('sinatra')
 require('sinatra/reloader')
-# require('./lib/task')
+require('./lib/patient')
 require('./lib/doctor')
 also_reload('lib/**/*.rb')
 require("pg")
@@ -25,4 +25,19 @@ end
 get('/doctors') do
   @doctors = Doctor.all()
   erb(:doctors)
+end
+
+get("/doctors/:id") do
+  @doctor = Doctor.find(params.fetch("id").to_i())
+  erb(:doctor)
+end
+
+post("/patients") do
+  name = params.fetch("name")
+  birthdate = params.fetch("birthdate")
+  doctor_id = params.fetch("doctor_id").to_i()
+  @doctor = Doctor.find(doctor_id)
+  @patient = Patient.new({:id => nil, :name => name, :birthdate => birthdate, :doctor_id => doctor_id})
+  @patient.save()
+  erb(:success2)
 end
